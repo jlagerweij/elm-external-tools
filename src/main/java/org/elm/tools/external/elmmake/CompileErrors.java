f5cd6eebe7704a1404c38a0dd12b427258dcbf5a
Jos Lagerweij <jos@lagerwey.net>
2018-09-17 22:54:20 +0200

    Fixes issue 6: Mix of Elm 0.19 and 0.18 code throws NullPointerExceptions

--- src/main/java/org/elm/tools/external/elmmake/CompileErrors.java
+++ src/main/java/org/elm/tools/external/elmmake/CompileErrors.java
@@ -5,4 +5,7 @@ import java.util.List;
 public class CompileErrors {
     public String type;
     public List<CompileError> errors;
+    public String path;
+    public String title;
+    public List<Object> message;
 }
--- src/main/java/org/elm/tools/external/elmmake/ElmMake.java
+++ src/main/java/org/elm/tools/external/elmmake/ElmMake.java
@@ -1,6 +1,5 @@
 package org.elm.tools.external.elmmake;
 
-import com.google.common.io.CharStreams;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
@@ -8,9 +7,14 @@ import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.ProcessNotCreatedException;
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 
 import org.elm.tools.external.ElmExternalToolsComponent;
 import org.jetbrains.annotations.NotNull;
@@ -21,12 +25,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class ElmMake {
     private static final Logger LOG = Logger.getInstance(ElmExternalToolsComponent.class);
+
+    private static final NotificationGroup GROUP_DISPLAY_ID_BALLOON =
+            new NotificationGroup("org.elmlang.intellijplugin.elmmake",
+                    NotificationDisplayType.BALLOON, false);
+    private static final String ELM_EXTERNAL_TOOLS = "Elm External Tools";
 
     private ElmMake() {
         // Private constructor to hide the implicit public one
@@ -41,15 +53,13 @@ public class ElmMake {
         commandLine.addParameter(file);
 
         try {
-            LOG.info(commandLine.getCommandLineString(elmMakeExePath));
+            LOG.info(workDirectory + ": " + commandLine.getCommandLineString(elmMakeExePath));
 
             Process process = commandLine.createProcess();
 
             process.waitFor();
             LOG.debug("Elm exit value: " + process.exitValue());
-            if (process.exitValue() == 1) {
-                return parseElmMakeOutput(process.getInputStream(), process.getErrorStream());
-            }
+            return parseElmMakeOutput(process.getInputStream(), process.getErrorStream());
 
         } catch (ExecutionException | InterruptedException e) {
             LOG.error(e.getMessage(), e);
@@ -57,20 +67,17 @@ public class ElmMake {
         return new ArrayList<>();
     }
 
-    public static List<Problems> parseElmMakeOutput(InputStream inputStream, InputStream errorStream) {
+    private static List<Problems> parseElmMakeOutput(InputStream inputStream, InputStream errorStream) {
         List<Problems> allProblems = new ArrayList<>();
         String output = null;
         try {
-            List<String> lines = CharStreams.readLines(new InputStreamReader(inputStream));
+            List<String> lines = readLines(new InputStreamReader(inputStream));
             if (lines.isEmpty()) {
-                List<String> errorLines = CharStreams.readLines(new InputStreamReader(errorStream));
+                List<String> errorLines = readLines(new InputStreamReader(errorStream));
                 if (notValidJson(errorLines)) {
                     String errorResult = String.join("\n", errorLines);
                     if (!errorResult.isEmpty()) {
-                        String groupDisplayId = "org.elmlang.intellijplugin.elmmake";
-                        String title = "Problem found performing Elm make";
-                        Notification notification = new Notification(groupDisplayId, title, errorResult, NotificationType.ERROR, null);
-                        Notifications.Bus.notify(notification);
+                        showNotification("Problem found performing Elm make", errorResult);
                     }
                 } else {
                     for (String line : errorLines) {
@@ -98,36 +105,88 @@ public class ElmMake {
         return allProblems;
     }
 
+    private static List<String> readLines(Reader r) throws IOException {
+        List<String> result = new ArrayList<>();
+        BufferedReader lineReader = new BufferedReader(r);
+
+        String line;
+        while ((line = lineReader.readLine()) != null) {
+            result.add(line);
+        }
+
+        return result;
+    }
+
+    private static void showNotification(String title, String message) {
+        ApplicationManager.getApplication().invokeLater(() -> {
+            Notification notification = GROUP_DISPLAY_ID_BALLOON.createNotification(ELM_EXTERNAL_TOOLS, title, message, NotificationType.ERROR);
+            Optional<Project> projects = Arrays.stream(ProjectManager.getInstance().getOpenProjects()).findFirst();
+            projects.ifPresent(project -> Notifications.Bus.notify(notification, project));
+        });
+    }
+
     private static List<Problems> parseCompileErrors(final String line) {
         List<Problems> problems = new ArrayList<>();
 
         final CompileErrors compileErrors = new Gson().fromJson(line, CompileErrors.class);
-        for (final CompileError error : compileErrors.errors) {
-            for (CompileProblem compileProblem : error.problems) {
-                StringBuilder details = new StringBuilder();
-                for (Object message : compileProblem.message) {
-                    if (message instanceof String) {
-                        details.append(message);
-                    } else if (message instanceof Map) {
-                        final Map messageMap = (Map) message;
-                        messageMap.get("bold");
-                        messageMap.get("underline");
-                        messageMap.get("color");
-                        final String string = (String) messageMap.get("string");
-                        details.append(string);
-                    }
-                }
+        if (compileErrors.errors != null) {
+            for (final CompileError error : compileErrors.errors) {
+                for (CompileProblem compileProblem : error.problems) {
+                    String detailsMessage = messagesToString(compileProblem.message);
 
-                final Problems problem = new Problems();
-                problem.file = error.path;
-                problem.type = compileErrors.type;
-                problem.region = compileProblem.region;
-                problem.overview = compileProblem.title;
-                problem.details = details.toString();
-                problems.add(problem);
+                    final Problems problem = new Problems();
+                    problem.file = error.path;
+                    problem.type = compileErrors.type;
+                    problem.region = compileProblem.region;
+                    problem.overview = compileProblem.title;
+                    problem.details = detailsMessage;
+                    problems.add(problem);
+                }
             }
         }
+        if ("error".equals(compileErrors.type) && compileErrors.message != null) {
+            showNotification(compileErrors.title, messagesToString(compileErrors.message));
+        }
         return problems;
+    }
+
+    @NotNull
+    private static String messagesToString(final List<Object> messages) {
+        StringBuilder details = new StringBuilder();
+        for (Object message : messages) {
+            if (message instanceof String) {
+                details.append(message);
+            } else if (message instanceof Map) {
+                final Map messageMap = (Map) message;
+                final boolean bold = Boolean.TRUE.equals(messageMap.get("bold"));
+                final boolean underline = Boolean.TRUE.equals(messageMap.get("underline"));
+                final String color = (String) messageMap.get("color");
+                final String string = (String) messageMap.get("string");
+
+                details.append(createStyledMessage(bold, underline, color, string));
+            }
+        }
+        return details.toString();
+    }
+
+    @NotNull
+    private static String createStyledMessage(final boolean bold, final boolean underline, final String color, final String string) {
+        String style = "<font ";
+        if (color != null) {
+            style += "color=\"" + color + "\" style=\"background-color:black; ";
+        } else {
+            style += "style=\"";
+        }
+        if (bold) {
+            style += "font-weight:bolder; ";
+        }
+        if (underline) {
+            style += "text-decoration: underline; ";
+        }
+        style += "\">";
+        style += string;
+        style += "</font>";
+        return style;
     }
 
     private static List<Problems> parseProblemJsonElm018(String output) {
